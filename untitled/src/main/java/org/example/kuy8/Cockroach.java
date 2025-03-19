3d62c20feefb36367c63ee21ca98dca2863d3f55
moongrail <innomars@yandex.ru>
2025-03-19 22:02:41 +0300

    Cockroach

---
+++ untitled/src/main/java/org/example/kuy8/Cockroach.java
@@ -0,0 +1,8 @@
+package org.example.kuy8;
+
+public class Cockroach {
+    public int cockroachSpeed(double x){
+        // Good Luck!
+        return (int) ((x*1000*100)/3600);
+    }
+}
---
+++ untitled/src/test/java/org/example/kuy8/CockroachTest.java
@@ -0,0 +1,16 @@
+package org.example.kuy8;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CockroachTest {
+    @Test
+    public void basicTests() throws Exception{
+        Cockroach cockroach = new Cockroach();
+        assertEquals(30, cockroach.cockroachSpeed(1.08));
+        assertEquals(30, cockroach.cockroachSpeed(1.09));
+        assertEquals(0, cockroach.cockroachSpeed(0));
+    }
+
+}
