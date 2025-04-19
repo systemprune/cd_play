6896b0a5223fffc3609fe8ae8c016878528c91f2
moongrail <innomars@yandex.ru>
2025-04-20 00:45:36 +0300

    ZywOo.java

---
+++ untitled/src/main/java/org/example/kuy8/ZywOo.java
@@ -0,0 +1,10 @@
+package org.example.kuy8;
+
+import java.util.regex.Pattern;
+
+public class ZywOo {
+    public static boolean validateUsr(String s) {
+        //glhf:)
+        return s.matches("^[a-z0-9_]{4,16}$");
+    }
+}
---
+++ untitled/src/test/java/org/example/kuy8/ZywOoTest.java
@@ -0,0 +1,21 @@
+package org.example.kuy8;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ZywOoTest {
+    @Test
+    public void basicTests() {
+        assertEquals(true, ZywOo.validateUsr("regex"));
+        assertEquals(false, ZywOo.validateUsr("a"));
+        assertEquals(false, ZywOo.validateUsr("Hass"));
+        assertEquals(false, ZywOo.validateUsr("Hasd_12assssssasasasasasaasasasasas"));
+        assertEquals(false, ZywOo.validateUsr(""));
+        assertEquals(true, ZywOo.validateUsr("____"));
+        assertEquals(false, ZywOo.validateUsr("012"));
+        assertEquals(true, ZywOo.validateUsr("p1pp1"));
+        assertEquals(false, ZywOo.validateUsr("asd43 34"));
+        assertEquals(true, ZywOo.validateUsr("asd43_34"));
+    }
+}
