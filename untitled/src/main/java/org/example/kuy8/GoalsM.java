fd12f04b62b945644d1f1a5a0e3023bee9d80046
moongrail <innomars@yandex.ru>
2025-06-18 00:01:59 +0300

    GoalsM.java

---
+++ untitled/src/main/java/org/example/kuy8/GoalsM.java
@@ -0,0 +1,9 @@
+package org.example.kuy8;
+
+public class GoalsM {
+    public static int laLigaGoals = 43;
+    public static int championsLeagueGoals = 10;
+    public static int copaDelReyGoals = 5;
+
+    public static int totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+}
---
+++ untitled/src/test/java/org/example/kuy8/GoalsMTest.java
@@ -0,0 +1,12 @@
+package org.example.kuy8;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GoalsMTest {
+    @Test
+    public void BasicTests() {
+        assertEquals(58, GoalsM.totalGoals);
+    }
+}
