02f12387da688e6d3f05dbcff75be4de08ff3114
moongrail <innomars@yandex.ru>
2025-06-22 10:32:36 +0300

    CSV.java

--- untitled/src/main/java/org/example/kuy8/CSV.java
+++ untitled/src/main/java/org/example/kuy8/CSV.java
@@ -1,13 +1,14 @@
 package org.example.kuy8;
 
-import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.util.Arrays.*;
+
 public class CSV {
     public static String toCsvText(int[][] array){
-        return Arrays.stream(array)
-                .flatMap(i -> Stream.of(Arrays.stream(i)
+        return stream(array)
+                .flatMap(i -> Stream.of(stream(i)
                             .mapToObj(String::valueOf)
                             .collect(Collectors.joining(",")))
                 )
