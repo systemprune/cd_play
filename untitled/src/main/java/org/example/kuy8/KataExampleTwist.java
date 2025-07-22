e721f765bc3b7d8f3c5cf534512dcb8edf26b685
moongrail <innomars@yandex.ru>
2025-07-23 00:08:24 +0300

    KataExampleTwist.java

---
+++ untitled/src/main/java/org/example/kuy8/KataExampleTwist.java
@@ -0,0 +1,11 @@
+package org.example.kuy8;
+
+import java.util.Arrays;
+
+public class KataExampleTwist {
+    public static String[] kataExampleTwist() {
+        String[] websites = new String[1000];
+        Arrays.fill(websites, "codewars");
+        return websites;
+    }
+}
