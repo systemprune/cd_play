740ec946a462d188987ce62df6bb7deba272ede8
moongrail <innomars@yandex.ru>
2025-08-19 19:38:49 +0300

    geom

---
+++ untitled/src/main/java/org/example/kuy8/Geometry.java
@@ -0,0 +1,8 @@
+package org.example.kuy8;
+
+public class Geometry {
+    public static double squareArea(double A){
+        //your code;
+        return Math.round(Math.pow((2 * A) / Math.PI, 2) * 100.0) / 100.0;
+    }
+}
