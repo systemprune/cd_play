8df5a00d9bbe121d052a136d95a87d26b8df8ff1
moongrail <innomars@yandex.ru>
2025-09-09 00:12:04 +0300

    SurfaceArea.java

---
+++ untitled/src/main/java/org/example/kuy8/SurfaceArea.java
@@ -0,0 +1,8 @@
+package org.example.kuy8;
+
+public class SurfaceArea {
+    public static int[] getSize(int w, int h, int d) {
+        //To-Do
+        return new int[]{((2 * w * h) + (2 * w * d) + (2 * h * d)), w * h * d};
+    }
+}
