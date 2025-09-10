6ed1ce2556b153c445ad83ed3b9262ea715606e1
moongrail <innomars@yandex.ru>
2025-09-11 00:09:57 +0300

    SmallestValueOfAnArray.java

---
+++ untitled/src/main/java/org/example/kuy7/SmallestValueOfAnArray.java
@@ -0,0 +1,22 @@
+package org.example.kuy7;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class SmallestValueOfAnArray {
+    public static int findSmallest(int[] numbers, String mode) {
+        int min = Arrays.stream(numbers).min().orElse(-1);
+        switch (mode) {
+            case "value" -> {
+                return min;
+            }
+            case "index" -> {
+                return IntStream.range(0, numbers.length)
+                        .filter(i -> numbers[i] == min)
+                        .findFirst()
+                        .orElse(-1);
+            }
+        }
+        return -1;
+    }
+}
